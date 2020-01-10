2b8469a176359f9e99ee591c99f232f36c85bdf8
Mikael <test.kurs@hotmail.net>
2020-01-09 16:14:20 -0800

    Add project files.

---
+++ EgenGymBooking/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EgenGymBooking.Core.ViewModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EgenGymBooking.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ EgenGymBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EgenGymBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
